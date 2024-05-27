a8bf1a52ce95100676a75d4a0dc2b75fd5854192
= <=>
2024-05-28 00:26:34 +0100

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,25 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10)
+console.log("Alexandre");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let alexandre_matilda = "AM";
+let $function = 27;
+
+let person = "alexandre";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Engineering Manager";
+
+let job1 = "web developer";
+let job2 = "engineering manager";
+
+console.log(myFirstJob);
