b8e9160b4f444b1e0a1ea3a4a036a432a1322d47
Alexandre Leprêtre <alexandre.lepretre88@gmail.com>
2024-05-30 00:32:53 +0100

    Assignment: let, const and var

---
+++ Assignments/03-let-const-var/script.js
@@ -0,0 +1,11 @@
+const country = 'United Kingdom';
+const continent = 'Europe';
+let population = 67;
+const isIsland = true;
+const language = 'English';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
