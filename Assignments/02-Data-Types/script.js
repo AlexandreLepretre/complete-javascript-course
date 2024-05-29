b800619f18402291e0a05a9d70c659cb26f6e1e0
= <=>
2024-05-29 23:31:04 +0100

    Assignment: Data Types

---
+++ Assignments/02-Data-Types/script.js
@@ -0,0 +1,11 @@
+let country = 'France';
+let continent = 'Europe';
+let population = 68;
+let isIsland = false;
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
